b178abb532f3cde8d7602ec1e7ea4df6c5752e69
Shubham Rao <shubhamrao834@gmail.com>
2024-05-04 01:20:19 +0530

    Complete the assignment

--- Readme.md
+++ Readme.md
@@ -1,45 +1,54 @@
+# Website URL:
+
+- https://66353d29d79e9e44b151f436--venerable-cheesecake-946e05.netlify.app/
+
 # Coding Assessment L-2 Code Submission Process with Boilerplate
 
 Welcome to the coding assessment l2. We are excited to see your skills in action. Please follow the instructions below to submit your solution.
 
 ## Getting Started
 
-1. **Clone or Download:**
-   - If using Git, clone the repository using the following command:
-     ```
-     git clone https://github.com/mmdmrishabh/Coding-assessment-l2-Boilerplate-code.git
-     ```
-   - Alternatively, download the code as a ZIP file and extract it.
+1.  **Clone or Download:**
 
-2. **Navigate to Project Directory:**
+    - If using Git, clone the repository using the following command:
+      ```
+      git clone https://github.com/mmdmrishabh/Coding-assessment-l2-Boilerplate-code.git
+      ```
+    - Alternatively, download the code as a ZIP file and extract it.
 
-3. **Explore the Structure:**
-Take a moment to review the project structure:
+2.  **Navigate to Project Directory:**
 
-        project-root/
-        |-- index.html
-        |-- style.css
-        |-- script.js
-        |-- README.md
+3.  **Explore the Structure:**
+    Take a moment to review the project structure:
+
+            project-root/
+            |-- index.html
+            |-- style.css
+            |-- script.js
+            |-- README.md
+
 - `index.html`: Main HTML file for your project.
 - `style.css`: CSS file for styling.
 - `script.js`: JavaScript file for scripting logic.
 - `README.md`: Documentation for your project.
-  
+
 > **Important:** Avoid using additional files or a single file for your code. Feel free to organize asset files by creating a separate folder.
 
 ## Development and Testing
 
 4. **Start Working:**
+
 - Read all requirements that are shared with design files.
 - Open `index.html` in your browser to view the initial state of the project, And start coding.
 - Modify the `style.css` and `script.js` files to implement your solution.
+
 5. **Testing:**
+
 - Test your code to ensure it produces the expected output.
 - Ensure that the console (Developer Tools) does not display any errors.
 
-
 ---
+
 ## Submission Guidelines
 
 ### Option 1: GitHub Repository
@@ -56,6 +65,7 @@ Take a moment to review the project structure:
 3. Please send the public link to the ZIP file in the same email thread where you received the task details.
 
 ## Hosting (Bonus)
+
 You can earn bonus points by hosting your project on a free hosting platform such as GitHub Pages or Netlify.
 
 ## Important Notes
--- script.js
+++ script.js
@@ -1,3 +1,73 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+let productDiv = document.querySelector(".product");
+
+let displayProducts = async () => {
+  productDiv.innerHTML = "";
+
+  try {
+    let response = await fetch(
+      "https://cdn.shopify.com/s/files/1/0564/3685/0790/files/multiProduct.json"
+    );
+
+    let data = await response.json();
+
+    // function to calculate discounted percentage:
+    const calculateDiscountPercentage = (price, compareAtPrice) => {
+      if (!compareAtPrice || compareAtPrice <= price) {
+        return null;
+      }
+      const discount = (compareAtPrice - price) / compareAtPrice;
+      return Math.round(discount * 100);
+    };
+
+    //Function to display product based on category:
+    const displayProductsByCategory = (categoryName) => {
+      productDiv.innerHTML = "";
+      const category = data.categories.find(
+        (category) => category.category_name === categoryName
+      );
+      if (category) {
+        category.category_products.forEach((product) => {
+          const discountPercentage = calculateDiscountPercentage(
+            parseFloat(product.price),
+            parseFloat(product.compare_at_price)
+          );
+
+          productDiv.innerHTML += `<div class="productItem">
+            <div class="img_container">
+              <div class="text">${product.badge_text}</div>
+              <img src="${product.image}" alt="">
+            </div>
+            <span class="name">
+              <h3>${product.title}</h3>
+              <ul class="vendor-list">
+                <li>${product.vendor}</li>
+              </ul>
+            </span>
+            <div class="price">
+              <p id="pri">${product.price} </p>
+              <p id="cpri">${product.compare_at_price} </p>
+              <p id="discount">(${discountPercentage}% off)</p>
+            </div>
+            <button class="btn_cart">Add to Cart</button>
+          </div>`;
+        });
+      } else {
+        alert("Category not found");
+      }
+    };
+
+    document.getElementById("menBtn").addEventListener("click", () => {
+      displayProductsByCategory("Men");
+    });
+    document.getElementById("womenBtn").addEventListener("click", () => {
+      displayProductsByCategory("Women");
+    });
+    document.getElementById("kidsBtn").addEventListener("click", () => {
+      displayProductsByCategory("Kids");
+    });
+  } catch (error) {
+    console.error("Error fetching or parsing data:", error);
+  }
+};
+
+displayProducts();
